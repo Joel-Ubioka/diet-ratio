ddcdb71fba20bed5bd4c2f44f06be556f14f24dc
Ify-tech <ifyugboko@gmail.com>
2022-04-06 18:01:54 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartsComponent } from './content/charts/charts.component';
+import { FoodListComponent } from './content/food-list/food-list.component';
+import { TodayComponent } from './content/today/today.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'charts', component:ChartsComponent},
+  { path: 'food-list', component:FoodListComponent },
+{ path: 'today', component:TodayComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FoodListComponent } from './content/food-list/food-list.component';
+import { TodayComponent } from './content/today/today.component';
+import { ChartsComponent } from './content/charts/charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FoodListComponent,
+    TodayComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
